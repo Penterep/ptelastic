d07c1fc196b87e86ef5324d1bf353a5661746110
Vít Stankuš <xstankus@fi.muni.cz>
2025-07-15 11:57:50 +0200

    Added documentation

--- ptelastic/modules/auth.py
+++ ptelastic/modules/auth.py
@@ -2,7 +2,11 @@
 Elasticsearch test to see if authentication is enabled
 
 This module implements a test that checks if an Elasticsearch instance has authentication
-enabled by sending a request to the server and checking if we get an HTTP response of 401
+enabled or disabled
+
+Contains:
+- Auth class for performing authentication test
+- run() function as an entry point for running the test
 """
 
 import http
@@ -13,6 +17,11 @@ __TESTLABEL__ = "Elasticsearch authentication test"
 
 
 class Auth:
+    """
+    This class tests to see if an Elasticsearch instance is running with authentication enabled or disabled by
+    sending a GET request to the provided URL and looking at the HTTP response code.
+    """
+
     def __init__(self, args: object, ptjsonlib: object, helpers: object, http_client: object, base_response: object) -> None:
         self.args = args
         self.ptjsonlib = ptjsonlib
@@ -23,8 +32,17 @@ class Auth:
         self.helpers.print_header(__TESTLABEL__)
 
     def run(self) -> None:
-        url = self.args.url
+        """
+        Executes the Elasticsearch authentication test
 
+        Sends one HTTP GET request to the provided URL and determines if authentication is enabled or not by the
+        HTTP response codes as follows:
+
+        401 Unauthorized = Authentication is enabled
+        200 OK - Authentication is disabled
+        """
+
+        url = self.args.url
         response = self.http_client.send_request(url, method="GET", headers=self.args.headers, allow_redirects=False)
 
         if self.args.verbose:
@@ -33,8 +51,10 @@ class Auth:
 
         if response.status_code == http.HTTPStatus.UNAUTHORIZED:
             ptprint(f"Authentication is enabled", "VULN", not self.args.json, indent=4)
+
         elif response.status_code == http.HTTPStatus.OK:
             ptprint(f"Authentication is disabled", "VULN", not self.args.json, indent=4)
+
 
 def run(args, ptjsonlib, helpers, http_client, base_response):
     """Entry point for running the Auth test"""
