e07a5d81f311099364a64d089c106e63c4146af4
d3b1t <vit.stankus@gmail.com>
2025-08-30 17:33:16 +0200

    Changed output argument

--- ptelastic/ptelastic.py
+++ ptelastic/ptelastic.py
@@ -265,7 +265,7 @@ def get_help():
             ["-F", "--file",                    "</path/to/file>",  "File to read if host is vulnerable to CVE-2015-5531 (default /etc/passwd)"],
             ["-di", "--dump-index"              "<index1, index2, ...>",      "Specify index to dump with data_dump module"],
             ["-df", "--dump-field",             "<field1,field2, field3.subfield>",     "Specify fields to dump with data_dump module"],
-            ["-o", "--out-file",                "<filename>",       "Specify the name of the file to store structure/data dump to"]
+            ["-o", "--output",                "<filename>",       "Specify the name of the file to store structure/data dump to"]
         ]
         }]
 
@@ -292,7 +292,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("-F", "--file",            type=str, default="/etc/passwd")
     parser.add_argument("-di", "--dump-index",     type=lambda f: f.split(","), default="")
     parser.add_argument("-df", "--dump-field",     type=lambda f: f.split(","), default=None)
-    parser.add_argument("-o", "--out-file",        type=lambda o: f"{o}.json", default=None)
+    parser.add_argument("-o", "--output",        type=lambda o: f"{o}.json", default=None)
     if len(sys.argv) == 1 or "-h" in sys.argv or "--help" in sys.argv:
         ptprint(help_print(get_help(), SCRIPTNAME, __version__))
         sys.exit(0)
